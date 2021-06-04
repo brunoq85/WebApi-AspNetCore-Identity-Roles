4b9c8e1364b22b5a94fd15ad2950b9af5dac4d5e
Bruno Quintella <bruno_q85@hotmail.com>
2021-06-04 02:41:53 -0300

    Criação da rotina de customização para associar uma role a uma claim

---
+++ Dev.Api/Controllers/RoleClaimsController.cs
@@ -0,0 +1,49 @@
+﻿using Dev.Api.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Dev.Api.Controllers
+{
+    [Route("api/associa-funcao-afirmacao", Name="Associa função/afirmação")]
+    [ApiController]
+    public class RoleClaimsController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public RoleClaimsController(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        [HttpPost("registrar")]
+        public async Task<ActionResult> CreateRoleAsync(RoleClaimsViewModel model)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            var role = await _roleManager.FindByIdAsync(model.RoleId);
+            var claim = new Claim(model.ClaimType, model.ClaimValue);
+
+            if (role == null) return BadRequest("A função informada não existe");            
+
+            var result = await _roleManager.AddClaimAsync(role, claim);
+
+            if (result.Succeeded)
+                return Ok($"A função {role.Name} foi associada a afirmação {claim.Type}");
+
+            foreach (var erro in result.Errors)
+            {
+                return BadRequest(erro.Description);
+            }
+
+            return BadRequest(model);
+        }
+    }
+}
---
+++ Dev.Api/ViewModels/RoleClaimsViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Dev.Api.ViewModels
+{
+    public class RoleClaimsViewModel
+    {
+        public string Id { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        public string RoleId { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 3)]
+        public string ClaimType { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 3)]
+        public string ClaimValue { get; set; }
+    }
+}
